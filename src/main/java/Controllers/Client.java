715d9e79e9da5610449be0ff438d5076a25639fd
Steve Birtles <sbirtles@farnborough.ac.uk>
2019-06-20 13:10:23 +0100

    Cleaned up redundant context request parameters.

--- src/main/java/Controllers/Client.java
+++ src/main/java/Controllers/Client.java
@@ -1,11 +1,9 @@
 package Controllers;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,35 +19,35 @@ public class Client {
     @GET
     @Path("img/{path}")
     @Produces({"image/jpeg,image/png"})
-    public byte[] getImageFile(@PathParam("path") String path, @Context HttpServletRequest request) {
+    public byte[] getImageFile(@PathParam("path") String path) {
         return getFile("client/img/" + path);
     }
 
     @GET
     @Path("js/{path}")
     @Produces({"text/javascript"})
-    public byte[] getJavaScriptFile(@PathParam("path") String path, @Context HttpServletRequest request) {
+    public byte[] getJavaScriptFile(@PathParam("path") String path) {
         return getFile("client/js/" + path);
     }
 
     @GET
     @Path("css/{path}")
     @Produces({"text/css"})
-    public byte[] getCSSFile(@PathParam("path") String path, @Context HttpServletRequest request) {
+    public byte[] getCSSFile(@PathParam("path") String path) {
         return getFile("client/css/" + path);
     }
 
     @GET
     @Path("{path}")
     @Produces({"text/html"})
-    public byte[] getIHTMLFile(@PathParam("path") String path, @Context HttpServletRequest request) {
+    public byte[] getIHTMLFile(@PathParam("path") String path) {
         return getFile("client/" + path);
     }
 
     @GET
     @Path("favicon.ico")
     @Produces({"image/x-icon"})
-    public byte[] getFavicon(@Context HttpServletRequest request) {
+    public byte[] getFavicon() {
         return getFile("client/favicon.ico");
     }
 
